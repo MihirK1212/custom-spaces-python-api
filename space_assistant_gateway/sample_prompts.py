7cabba7f3f8d389c286c0fd38fe6335aa2152b20
Mihir Karandikar <85657483+MihirK1212@users.noreply.github.com>
2025-12-29 20:13:26 +0530

    Widgets agent (#5)
    Widgets agent

---
+++ space_assistant_gateway/sample_prompts.py
@@ -0,0 +1,15 @@
+"""
+<USER>Fetch all my todo items. </USER> <API_PARAMETERS> {widgetId: 690e2f69de08bc3bed3ce385} </API_PARAMETERS>
+"""
+
+"""
+<USER>Create a new todo item to remind me to buy a new phone </USER> <API_PARAMETERS> {widgetId: 690e2f69de08bc3bed3ce385} </API_PARAMETERS>
+"""
+
+"""
+Fetch the todo items for the widgetId=690e2f69de08bc3bed3ce385. {widgetId: 690e2f69de08bc3bed3ce385}
+"""
+
+"""
+add a todo_list widget for tracking 'Goa Plan' inside spaceId=68f7838274710931d1c342b5. {spaceId: 68f7838274710931d1c342b5} {widgetType: todo_list, displayName: 'Goa Plan'}
+"""
