661bc6b88c0a72b1002eeab219a02ee54f7e9790
Mihir Karandikar <mihirkkarandikar@gmail.com>
2026-01-10 18:53:08 +0530

    add prompt

--- space_assistant_gateway/sample_prompts.py
+++ space_assistant_gateway/sample_prompts.py
@@ -13,3 +13,13 @@ Fetch the todo items for the widgetId=690e2f69de08bc3bed3ce385. {widgetId: 690e2
 """
 add a todo_list widget for tracking 'Goa Plan' inside spaceId=68f7838274710931d1c342b5. {spaceId: 68f7838274710931d1c342b5} {widgetType: todo_list, displayName: 'Goa Plan'}
 """
+
+"""
+Curate this space for my Goa Trip that i am planning with my friends 
+Use your discretion to create the optimal widgets for the space, but the general idea is:
+Money split widget to track the expenses of the trip 
+Multiple todo lists to track tasks across different categories such as a bucket list, packing list, things to do, booking related tasks etc 
+Multiple chats based on use case such as a general chat, a chat for accomodation bookings, a chat for memes 
+
+No need to stick to this strictly. Just create the most optimal space for me
+"""
